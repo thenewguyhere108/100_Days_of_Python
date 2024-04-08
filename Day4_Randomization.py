4774667d1554f400462af5ee252fa205cb85dfaa
Sri Sandeep <sandyorton7@gmail.com>
2024-04-08 13:45:40 +0530

    Add : Day 4 Exercise Rock Paper and Scissors

---
+++ Day4_Randomization.py
@@ -0,0 +1,46 @@
+import random as ra
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+print("What do you choose? Type 0 for Rock , Type 1 for Paper, Type 2 for Scissors")
+b = int(input())
+a = [rock, paper, scissors]
+print(a[b])
+print('Computer Chose:')
+choice = ra.randint(0, 2)
+print(a[choice])
+if b == choice:
+    print("Game Draw")
+elif b == 0 and choice == 2:
+    print("You Win")
+elif b == 1 and choice == 0:
+    print("You Win")
+elif b == 2 and choice == 1:
+    print("You Win")
+else:
+    print("You Lose")
