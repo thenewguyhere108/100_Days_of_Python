d1853d92344d6e6c5da219cac649dcb417de8d97
Sri Sandeep <sandyorton7@gmail.com>
2024-04-13 00:18:48 +0530

    Add Day 14 : Higher vs Lower

---
+++ Day 14 Higher Lower/main.py
@@ -0,0 +1,44 @@
+from assets import logo, vs, clear, sleep
+from game_data import data
+from random import randint
+
+def compare(A,B):
+    if A['follower_count'] > B['follower_count']:
+        return 0
+    else:
+        return 1
+
+
+while True:
+    score = 0
+    clear()
+    print(logo,'\n')
+    A = data[randint(0,len(data)-1)]
+    while True:
+        while True:
+            B = data[randint(0,len(data)-1)]
+            if B != A:
+                break
+        if score >= 1:
+            clear()
+            print(logo)
+            print(f'Your score is :{score}')
+        print(f"Compare A: {A['name']}, {A['description']}, from {A['country']}")
+        print(vs,'\n')
+        print(f"Compare A: {B['name']}, {B['description']}, from {B['country']} \n")
+        while True:
+            choice = input("Who has more followers? A or B : ").lower()
+            if choice != 'a' and choice !='b':
+                print('Invalid input , try again')
+            else:
+                break
+        result = compare(A,B)
+        if ( result == 0 and choice == 'a' ) or (result == 1 and choice == 'b'):
+            score += 1
+            A = B 
+        else:
+            print(f'You lost , Your Score {score}')
+            break
+    choice = input('Press y to play again or any other key to quit').lower()
+    if choice != 'y':
+        break
