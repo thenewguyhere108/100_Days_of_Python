b9551ffe51d0f4ea352ec6bea27e734c726d08e6
Sri Sandeep <sandyorton7@gmail.com>
2024-04-22 22:53:23 +0530

    Day 25: US States Game update

--- Day 25 US States Pandas/main.py
+++ Day 25 US States Pandas/main.py
@@ -8,6 +8,7 @@ score = 0
 answers = []
 data = pd.read_csv("50_states.csv")
 data_list = data.set_index("state").T.to_dict("list")
+missing_states = []
 
 
 def create_turtle(x, y, name):
@@ -17,14 +18,20 @@ def create_turtle(x, y, name):
     tim.penup()
     tim.goto(x, y)
     tim.write(name, align='center', font=("Casper", 8, "normal"))
-    answers.append(tim)
 
 
 while score < 50:
     choice = screen.textinput(title=f"{score}/50 Guess the state", prompt="Enter the name of a state")
     choice = choice.title()
+    if choice == "Exit":
+        for i in data_list.keys():
+            if i not in answers:
+                missing_states.append(i)
+        df = pd.DataFrame(missing_states)
+        df.to_csv("missing_states.csv")
+        break
     if choice in data_list.keys():
+        answers.append(choice)
         score += 1
         create_turtle(data_list[choice][0], data_list[choice][1], choice)
-
 turtle.mainloop()
